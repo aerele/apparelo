fab47020a48664bc606024baab14318169559b8d
kaviya.3111999@gmail.com <kaviya.3111999@gmail.com>
2019-12-18 12:23:52 +0530

    updated ipd_doctype

--- apparelo/apparelo/doctype/ironing/ironing.py
+++ apparelo/apparelo/doctype/ironing/ironing.py
@@ -9,6 +9,33 @@ from frappe.model.document import Document
 class Ironing(Document):
 	def on_submit(self):
 		create_item_template(self)
+	def create_variants(self, input_item_names):
+		input_items = []
+		for input_item_name in input_item_names:
+			input_items.append(frappe.get_doc('Item', input_item_name))
+		attribute_set = get_item_attribute_set(list(map(lambda x: x.attributes, input_items)))
+		variants = create_variants('Ironed Cloth', attribute_set)
+		return list(set(variants))
+	def create_boms(self, input_item_names, variants):
+		item_list = []
+		boms = []
+		for input_item in input_item_names:
+			item_list.append({"item_code": input_item,"uom": "Nos"})
+		existing_bom = frappe.db.get_value('BOM', {'item': variants[0]}, 'name')
+		if not existing_bom:
+			bom = frappe.get_doc({
+				"doctype": "BOM",
+				"currency": get_default_currency(),
+				"item": variants[0],
+				"company": get_default_company(),
+				"items": item_list
+			})
+			bom.save()
+			bom.submit()
+			boms.append(bom.name)
+		else:
+			boms.append(existing_bom)
+		return boms
 
 def create_item_template(self):
 	# todo: need to check if an item already exists with the same name
