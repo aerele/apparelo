5c93a02749ad5fb7e563e48cdee6662d9dde1d7b
indhu <indhu1999saisri@gmail.com>
2019-12-18 12:31:28 +0530

    updated packing doctype..

--- apparelo/apparelo/doctype/packing/packing.py
+++ apparelo/apparelo/doctype/packing/packing.py
@@ -5,10 +5,46 @@
 from __future__ import unicode_literals
 import frappe
 from frappe.model.document import Document
+from apparelo.apparelo.utils.item_utils import get_attr_dict, get_item_attribute_set, create_variants
+from erpnext.controllers.item_variant import generate_keyed_value_combinations, get_variant
+from erpnext import get_default_company, get_default_currency
 
 class Packing(Document):
 	def on_submit(self):
 		create_item_template(self)
+
+	def create_variants(self, input_item_names):
+		input_items = []
+		for input_item_name in input_item_names:
+			input_items.append(frappe.get_doc('Item', input_item_name))
+		attribute_set = get_item_attribute_set(list(map(lambda x: x.attributes, input_items)))
+		variants = create_variants('Packed Cloth', attribute_set)
+		return list(set(variants))
+
+	def create_boms(self, input_item_names, variants):
+		item_list = []
+		boms = []
+		for input_item in input_item_names:
+			item_list.append({"item_code": input_item,"uom": "Nos"})
+		print("**************************")
+		print(self.additional_parts)
+		print("*************************")
+		existing_bom = frappe.db.get_value('BOM', {'item': variants[0]}, 'name')
+		if not existing_bom:
+			bom = frappe.get_doc({
+				"doctype": "BOM",
+				"currency": get_default_currency(),
+				"item": variants[0],
+				"company": get_default_company(),
+				"items": item_list
+			})
+			bom.save()
+			bom.submit()
+			boms.append(bom.name)
+		else:
+			boms.append(existing_bom)
+		return boms
+
 
 def create_item_template(self):
 	# todo: need to check if an item already exists with the same name
@@ -23,26 +59,7 @@ def create_item_template(self):
 		"variant_based_on" : "Item Attribute",
 		"attributes" : [
 			{
-				"attribute" : "Yarn Shade" 
-			},
-			{
-				"attribute" : "Yarn Category"
-			},
-			{
-			
-				"attribute" : "Yarn Count"
-			},
-			{
-				"attribute" : "Dia" 
-			},
-			{
-				"attribute" : "Knitting Type"
-			},
-			{
 				"attribute" : "Apparelo Colour" 
-			},
-			{
-				"attribute" : "Part" 
 			},
 			{
 				"attribute" : "Apparelo Size"
