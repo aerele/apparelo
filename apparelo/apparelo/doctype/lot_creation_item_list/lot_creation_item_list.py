ca6396c14249c40cff5837925616997be16a9291
Haris2032 <mohammedharis.k.01@gmail.com>
2019-12-24 11:02:27 +0530

    update lot-creation

---
+++ apparelo/apparelo/doctype/lot_creation_item_list/lot_creation_item_list.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+# Copyright (c) 2019, Aerele Technologies Private Limited and contributors
+# For license information, please see license.txt
+
+from __future__ import unicode_literals
+# import frappe
+from frappe.model.document import Document
+
+class LotCreationItemList(Document):
+	pass
