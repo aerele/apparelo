eb587ce84b75c39fe33612ece5416200c446ae1a
kaviya.3111999@gmail.com <kaviya.3111999@gmail.com>
2019-12-25 15:02:34 +0530

    auto_populate_items_and_boms

--- apparelo/apparelo/doctype/lot_creation/lot_creation.py
+++ apparelo/apparelo/doctype/lot_creation/lot_creation.py
@@ -3,9 +3,10 @@
 # For license information, please see license.txt
 
 from __future__ import unicode_literals
-import frappe
+import frappe, json
 from frappe.model.document import Document
-
+from six import string_types, iteritems
+from erpnext.manufacturing.doctype.work_order.work_order import get_item_details
 class LotCreation(Document):
 	def on_submit(self):
 		default_company = frappe.db.get_single_value('Global Defaults', 'default_company')
@@ -13,7 +14,18 @@ class LotCreation(Document):
 		parent = "Lot Warehouse"
 		create_parent_warehouse(parent,abbr)
 		create_warehouse(self,parent,abbr)
-
+@frappe.whitelist()
+def get_ipd_item(doc):
+	if isinstance(doc, string_types):
+		doc = frappe._dict(json.loads(doc))
+	doc['po_items'] = []
+	po_items=[]
+	item_production_detail=doc.get('item_production_detail')
+	item_template=frappe.db.get_value("Item Production Detail",{'name': item_production_detail},"item")
+	item_code=frappe.db.get_all("Item", fields=["item_code"], filters={"variant_of": item_template})
+	for item in item_code:
+		po_items.append({"item_code":item.get("item_code"),"bom_no":get_item_details(item.get("item_code")).get("bom_no")})
+	return po_items
 def create_parent_warehouse(name,abbr):
 	if not frappe.db.exists("Warehouse",f'{name} - {abbr}'):
 		frappe.get_doc({
