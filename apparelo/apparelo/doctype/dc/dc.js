37b40ab081add74e78613b76a11f90880f1f54e6
kaviya.3111999@gmail.com <kaviya.3111999@gmail.com>
2019-12-27 20:50:19 +0530

    Supplier filteration

--- apparelo/apparelo/doctype/dc/dc.js
+++ apparelo/apparelo/doctype/dc/dc.js
@@ -2,7 +2,14 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('DC', {
-	// refresh: function(frm) {
-
-	// }
+	onload: function(frm) {
+		frm.set_query("supplier", function() {
+			return {
+				query: "apparelo.apparelo.doctype.dc.dc.get_supplier",
+				filters: {
+					"supplier_process.processes": frm.doc.process_1
+				}
+			};
+		});
+	}
 });
--- apparelo/apparelo/doctype/dc/dc.py
+++ apparelo/apparelo/doctype/dc/dc.py
@@ -4,6 +4,7 @@
 
 from __future__ import unicode_literals
 <<<<<<< HEAD
+<<<<<<< HEAD
 import frappe
 from frappe.model.document import Document
 from frappe.custom.doctype.custom_field.custom_field import create_custom_fields
@@ -105,8 +106,45 @@ def make_custom_fields(update=True):
 	create_custom_fields(custom_fields,ignore_validate = frappe.flags.in_patch, update=update)
 =======
 # import frappe
+=======
+import frappe
+>>>>>>> 8848caf... Supplier filteration
 from frappe.model.document import Document
+from frappe.utils import cstr, flt, cint, nowdate, add_days, comma_and, now_datetime, ceil
 
 class DC(Document):
+<<<<<<< HEAD
 	pass
 >>>>>>> 6e84224... DC doctype creation:
+=======
+	def onsubmit(self):
+		create purchase_order(self)
+		create stock_entry(self)
+def create purchase_order(self):
+	schedule_date = add_days(nowdate(), cint(item_doc.lead_time_days))
+	frappe.get_doc({
+		"doctype": "Purchase Order",
+		"docstatus": 1, 
+		"supplier": self.supplier, 
+		"schedule_date": schedule_date,
+		"set_warehouse": \"$ware - JPR\", 
+		"is_subcontracted": "Yes", 
+		\"supplier_warehouse\": \"$sName - JPR\", 
+		"items": [ { "item_code": \"$variant-$iname\",
+		"qty": $qty } ], 
+		"supplied_items": [ { \"main_item_code\": \"$variant-$iname\", 
+		\"rm_item_code\": \"$variant-$iName\", 
+		\"required_qty\":$qty, 
+		\"reserve_warehouse\": \"$ware - JPR\"} ] }).save().submit()
+def create stock_entry(self):
+	frappe.get_doc().save().submit()
+def get_supplier(doctype, txt, searchfield, start, page_len, filters):
+	suppliers=[]
+	all_supplier=frappe.db.get_all("Supplier")
+	for supplier in all_supplier:
+		process_supplier=frappe.get_doc("Supplier",supplier.name)
+		for process in process_supplier.supplier_process:
+			if process.processes==filters['supplier_process.processes']:
+				suppliers.append([supplier.name])
+	return suppliers
+>>>>>>> 8848caf... Supplier filteration
