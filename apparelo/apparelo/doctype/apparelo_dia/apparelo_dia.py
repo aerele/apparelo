59a68a4d897314ce5f602f2af8c3bb4b498eb7f1
Vignesh S <svignesh0308@gmail.com>
2020-01-25 12:50:13 +0530

    Add apparelo Dia

--- apparelo/apparelo/doctype/apparelo_dia/apparelo_dia.py
+++ apparelo/apparelo/doctype/apparelo_dia/apparelo_dia.py
@@ -3,8 +3,45 @@
 # For license information, please see license.txt
 
 from __future__ import unicode_literals
-# import frappe
+import frappe
 from frappe.model.document import Document
 
 class AppareloDia(Document):
-	pass
+	def validate(self):
+		self.append_to_item_attribute()
+
+	def append_to_item_attribute(self):
+		item_attribute = frappe.get_doc("Item Attribute","Dia")
+		dias=[]
+
+		for value in item_attribute.item_attribute_values:
+			types.append(value.attribute_value)
+		
+		if not self.dia in dias:
+			item_attribute.append('item_attribute_values',{
+				"attribute_value" : self.dia,
+				"abbr" : self.dia
+			})
+		item_attribute.save()
+
+def populate():
+		if not frappe.db.exists("Item Attribute", "Dia"):
+			item_attribute = frappe.get_doc({
+				"doctype": "Item Attribute",
+				"attribute_name": "Dia",
+				"item_attribute_values": []
+			})
+			for i in range(40, 165, 1):
+				i = i * 0.25
+				if int(str(i).split('.')[1]) == 0:
+					i = str(i).split('.')[0]
+				elif int(str(i).split('.')[1]) == 5:
+						i = str(i) + '0'
+				apparelo_dia = frappe.new_doc("Apparelo Dia")
+				apparelo_dia.dia = str(i)
+				apparelo_dia.save()
+				item_attribute.append('item_attribute_values',{
+					"attribute_value" : str(i),
+					"abbr" : str(i)
+				})
+			item_attribute.save()
