9748f00021669fb4c3fa5d23fa76fba46257cf53
Vignesh S <svignesh0308@gmail.com>
2020-01-25 13:40:55 +0530

    Fix intendation issue

--- apparelo/apparelo/doctype/apparelo_dia/apparelo_dia.py
+++ apparelo/apparelo/doctype/apparelo_dia/apparelo_dia.py
@@ -25,23 +25,23 @@ class AppareloDia(Document):
 		item_attribute.save()
 
 def populate():
-		if not frappe.db.exists("Item Attribute", "Dia"):
-			item_attribute = frappe.get_doc({
-				"doctype": "Item Attribute",
-				"attribute_name": "Dia",
-				"item_attribute_values": []
+	if not frappe.db.exists("Item Attribute", "Dia"):
+		item_attribute = frappe.get_doc({
+			"doctype": "Item Attribute",
+			"attribute_name": "Dia",
+			"item_attribute_values": []
+		})
+		for i in range(40, 165, 1):
+			i = i * 0.25
+			if int(str(i).split('.')[1]) == 0:
+				i = str(i).split('.')[0]
+			elif int(str(i).split('.')[1]) == 5:
+					i = str(i) + '0'
+			apparelo_dia = frappe.new_doc("Apparelo Dia")
+			apparelo_dia.dia = str(i)
+			apparelo_dia.save()
+			item_attribute.append('item_attribute_values',{
+				"attribute_value" : str(i),
+				"abbr" : str(i)
 			})
-			for i in range(40, 165, 1):
-				i = i * 0.25
-				if int(str(i).split('.')[1]) == 0:
-					i = str(i).split('.')[0]
-				elif int(str(i).split('.')[1]) == 5:
-						i = str(i) + '0'
-				apparelo_dia = frappe.new_doc("Apparelo Dia")
-				apparelo_dia.dia = str(i)
-				apparelo_dia.save()
-				item_attribute.append('item_attribute_values',{
-					"attribute_value" : str(i),
-					"abbr" : str(i)
-				})
-			item_attribute.save()
+		item_attribute.save()
