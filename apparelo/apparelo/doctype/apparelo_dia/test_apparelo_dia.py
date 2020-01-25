3f3ce976ca1eaa6669d25d198122dedc61f3a9cd
Vignesh S <svignesh0308@gmail.com>
2020-01-25 11:00:30 +0530

    Add apparelo dia doctype

---
+++ apparelo/apparelo/doctype/apparelo_dia/test_apparelo_dia.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+# Copyright (c) 2020, Aerele Technologies Private Limited and Contributors
+# See license.txt
+from __future__ import unicode_literals
+
+# import frappe
+import unittest
+
+class TestAppareloDia(unittest.TestCase):
+	pass
