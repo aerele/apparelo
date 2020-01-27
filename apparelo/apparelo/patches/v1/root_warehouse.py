9c5bb7a26450b88e7a984d7949f2fe8e8e246d5a
Haris2032 <mohammedharis.k.01@gmail.com>
2020-01-27 15:41:36 +0530

    Rename create_lot_warehouse to create_root_warehouse

--- apparelo/apparelo/patches/v1/root_warehouse.py
+++ apparelo/apparelo/patches/v1/root_warehouse.py
@@ -2,5 +2,5 @@ from __future__ import unicode_literals
 import frappe
 
 def execute():
-    from apparelo.install import create_lot_warehouse
-    create_lot_warehouse()
+    from apparelo.install import create_root_warehouse
+    create_root_warehouse()
