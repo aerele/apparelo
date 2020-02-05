67e441513cead4781b6b90eb52a22b2c447f544e
Mohammed Haris <mohammedharis.k.01@gmail.com>
2020-02-05 16:39:34 +0530

    Create Surplus warehouse

--- apparelo/install.py
+++ apparelo/install.py
@@ -16,6 +16,7 @@ from apparelo.apparelo.doctype.dc.dc import make_item_fields
 from apparelo.apparelo.doctype.additional_parameters import additional_parameters
 from apparelo.apparelo.doctype.apparelo_dia import apparelo_dia
 
+
 def after_install():
     remove_defaults()
     create_item_group()
@@ -27,11 +28,13 @@ def after_install():
     make_custom_fields()
     create_root_warehouse()
 
+
 def create_item_attributes():
     knitting.create_item_attribute()
     dyeing.create_item_attribute()
     cutting.create_item_attribute()
     roll_printing.create_item_attribute()
+
 
 def create_item_template():
     knitting.create_item_template()
@@ -41,27 +44,33 @@ def create_item_template():
     steaming.create_item_template()
     roll_printing.create_item_template()
 
+
 def create_attr_values():
     knitting.create_attr_values()
     knitting.create_additional_attribute()
     additional_parameters.create_parameter()
     apparelo_dia.populate()
 
+
 def remove_defaults():
-    stock_setting=frappe.get_doc("Stock Settings")
-    stock_setting.stock_uom=None
+    stock_setting = frappe.get_doc("Stock Settings")
+    stock_setting.stock_uom = None
     stock_setting.save()
 
+
 def create_new_uom():
-    if not frappe.db.exists("UOM","Combined Part"):
-        uom=frappe.new_doc("UOM")
-        uom.uom_name= 'Combined Part'
+    if not frappe.db.exists("UOM", "Combined Part"):
+        uom = frappe.new_doc("UOM")
+        uom.uom_name = 'Combined Part'
         uom.save()
 
+
 def create_root_warehouse():
-    default_company = frappe.db.get_single_value('Global Defaults', 'default_company')
+    default_company = frappe.db.get_single_value(
+        'Global Defaults', 'default_company')
     abbr = frappe.db.get_value("Company", f"{default_company}", "abbr")
-    warehouses = ["Lot Warehouse","Supplier Warehouse"]
+    warehouses = ["Lot Warehouse", "Supplier Warehouse",
+                  "Surplus Warehouse", "Surplus Mistake Warehouse"]
     for name in warehouses:
         if not frappe.db.exists("Warehouse", f'{name} - {abbr}'):
             frappe.get_doc({
