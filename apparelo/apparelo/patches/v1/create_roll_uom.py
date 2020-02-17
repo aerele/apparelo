0c869fe2fe156675d40da96e420bef52cd3517a8
Mohammed Haris <mohammedharis.k.01@gmail.com>
2020-02-17 11:04:56 +0530

    Reload apparelo doctype

--- apparelo/apparelo/patches/v1/create_roll_uom.py
+++ apparelo/apparelo/patches/v1/create_roll_uom.py
@@ -4,5 +4,6 @@ import frappe
 def execute():
     from apparelo.install import create_roll_uom
     from apparelo.apparelo.doctype.apparelo_process.apparelo_process import create_apparelo_process
+    frappe.reload_doc('Apparelo', 'doctype', 'Apparelo Process')
     create_roll_uom()
     create_apparelo_process()
