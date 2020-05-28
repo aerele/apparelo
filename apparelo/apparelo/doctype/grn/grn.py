1d980ce6b4f1cdb7d254df1252c0e055f0940f53
KaviyaPeriyasamy <kaviyaperiyasamy22@gmail.com>
2020-05-28 22:27:46 +0530

    Helper sections are added in GRN.

--- apparelo/apparelo/doctype/grn/grn.py
+++ apparelo/apparelo/doctype/grn/grn.py
@@ -8,6 +8,8 @@ from frappe import _,msgprint
 from frappe.model.document import Document
 from six import string_types, iteritems
 from frappe.utils import cstr, flt, cint, nowdate, add_days, comma_and, now_datetime, ceil
+from apparelo.apparelo.utils.item_utils import get_item_attribute_set
+import itertools
 
 
 class GRN(Document):
@@ -75,6 +77,21 @@ def get_items(doc):
 	PO=frappe.get_doc("Purchase Order",doc_)
 	if dc_process:
 		apparelo_process=frappe.get_doc("Apparelo Process",dc_process)
+	pr_list = frappe.get_list(
+		"Purchase Receipt Item",
+		filters={'purchase_order': ['in', doc_]},
+		fields=['parent'], group_by='parent'
+		)
+	if pr_list:
+		pr_names = [pr["parent"] for pr in pr_list]
+		pr_items = frappe.get_list("Purchase Receipt", filters={'name': ['in',pr_names]}, fields=["`tabPurchase Receipt Item`.item_code","`tabPurchase Receipt Item`.received_qty"]) 
+		for key, group in itertools.groupby(pr_items, key=lambda x: (x['item_code'])):
+			total_received_qty = 0 
+			for item in group:
+				total_received_qty+=item['received_qty']
+			for item in PO.items:
+				if item.item_code == key:
+					item.qty-=total_received_qty
 	for item in PO.items:
 		item_detail = frappe.get_doc('Item', item.item_code)
 		if apparelo_process:
@@ -82,3 +99,43 @@ def get_items(doc):
 		else:
 			return_materials.append({"item_code":item.item_code,"uom":item.uom,"qty":item.qty,"pf_item_code":item_detail.print_code,"secondary_uom":item.uom})
 	return return_materials
+
+@frappe.whitelist()
+def divide_total_quantity(doc):
+	return_materials = []
+	total_expected_qty = 0
+	matching_item_list = []
+	if isinstance(doc, string_types):
+		doc = frappe._dict(json.loads(doc))
+	attribute = doc.get('attribute')
+	attribute_value = doc.get('attribute_value')
+	total_received_qty = doc.get('total_quantity_received')
+	if total_received_qty:
+		for return_material in doc['return_materials']:
+			item_doc = frappe.get_doc("Item",return_material['item_code'])
+			attribute_set = get_item_attribute_set(list(map(lambda x: x.attributes,[item_doc])))
+			if attribute_set[attribute][0] == attribute_value:
+				total_expected_qty += return_material['qty']
+				matching_item_list.append(return_material['item_code'])
+		for return_item in doc['return_materials']:
+			if return_item['item_code'] in matching_item_list:
+				if total_received_qty>total_expected_qty:
+					remaining_qty = total_received_qty - total_expected_qty
+					return_item['received_qty'] = return_item['qty'] + (remaining_qty/total_expected_qty)*return_item['qty']
+				else:
+					return_item['received_qty'] = (total_received_qty/total_expected_qty)*return_item['qty']
+				return_materials.append({"item_code":return_item['item_code'],"uom":return_item['uom'],"qty":return_item['qty'],"pf_item_code":return_item['pf_item_code'],"secondary_uom":return_item['secondary_uom'],"received_qty":return_item['received_qty']})
+			else:
+				if 'received_qty' in return_item:
+					return_materials.append({"item_code":return_item['item_code'],"uom":return_item['uom'],"qty":return_item['qty'],"pf_item_code":return_item['pf_item_code'],"secondary_uom":return_item['secondary_uom'],"received_qty":return_item['received_qty']})
+				else:
+					return_materials.append({"item_code":return_item['item_code'],"uom":return_item['uom'],"qty":return_item['qty'],"pf_item_code":return_item['pf_item_code'],"secondary_uom":return_item['secondary_uom']})
+	return return_materials
+
+@frappe.whitelist()
+def get_attribute_value(attribute):
+	attribute_value_list = []
+	attr_doc = frappe.get_doc("Item Attribute",attribute)
+	for attr in attr_doc.item_attribute_values:
+		attribute_value_list.append([attr.attribute_value])
+	return attribute_value_list
