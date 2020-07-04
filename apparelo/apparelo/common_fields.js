e89be23b405599375f3baea76abea74c09a0ea92
Kaviya Periyasamy <kaviya.3111999@gmail.com>
2020-07-04 12:15:11 +0530

    uncombined parts filter added in other doctype part fileds

--- apparelo/apparelo/common_fields.js
+++ apparelo/apparelo/common_fields.js
@@ -2,6 +2,13 @@
 // For license information, please see license.txt
 frappe.ui.form.on(cur_frm.doctype,{
 onload: function(frm) {
+    frm.set_query("part", function() {
+        return {
+            filters: {
+                "is_combined":0
+            }
+        };
+    });
     frm.set_query("item", function() {
         return {
             "filters":{
@@ -48,7 +55,7 @@ onload: function(frm) {
             }
         };
     });
-    if(cur_frm && cur_frm.doctype==='Stitching'){
+    if(cur_frm && cur_frm.doctype!=='Cutting'){
     frm.set_query("part", "colour_mappings", function() {
         return {
             filters: {
@@ -70,6 +77,6 @@ onload: function(frm) {
             }
         };
     });
-}
+    }
 }
 });
